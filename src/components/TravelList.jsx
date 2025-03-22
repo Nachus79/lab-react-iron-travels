749f85fa601f3c7a2dae62b003e341a39e611812
Nacho Monzón <ignaciomonzona@gmail.com>
2025-03-22 13:13:24 +0100

    Iteration 2 complete

--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -4,14 +4,37 @@ import travelPlansData from "../assets/travel-plans.json";
 function TravelList() {
   return (
     <div>
+      <h1>Travel Plans</h1>
       <div className="travel-list">
         {travelPlansData.map((plan) => (
           <div key={plan.id} className="travel-card">
-            <img src={plan.image} alt={plan.destination} className="travel-image" />
+            <img
+              src={plan.image}
+              alt={plan.destination}
+              className="travel-image"
+            />
             <div className="travel-info">
-              <h3>{plan.destination} ({plan.days} Days)</h3>
-              <p><em>{plan.description}</em></p>
-              <p><b>Price:</b> {plan.totalCost}€</p>
+              <h3>
+                <em>{plan.destination}</em> ({plan.days} Days)
+              </h3>
+              <p>
+                <em>{plan.description}</em>
+              </p>
+              <p>
+                <strong>Price:</strong> {plan.totalCost}€
+              </p>
+
+              <div className="labels">
+                {plan.totalCost <= 350 && (
+                  <span className="label label-deal">Great Deal</span>
+                )}
+                {plan.totalCost >= 1500 && (
+                  <span className="label label-premium">Premium</span>
+                )}
+                {plan.allInclusive && (
+                  <span className="label label-inclusive">All Inclusive</span>
+                )}
+              </div>
             </div>
           </div>
         ))}
@@ -21,4 +44,5 @@ function TravelList() {
 }
 
 export default TravelList;
+
 
